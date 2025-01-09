479103f18f43c485f26774b0a30a0c6617dc2337
Keaton Lee <keaton.lee@bcbsks.com>
2025-01-09 17:25:04 -0600

    First Commit

---
+++ ViewModels/StoreViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using DynamicData.Binding;
+using ReactiveUI;
+
+namespace ServerSideExample.ViewModels
+{
+    public class StoreViewModel : ReactiveObject
+    {
+        private IObservableCollection<Department> _department;
+
+        public IObservableCollection<Department> Departments
+        {
+            get => _department;
+            set => this.RaiseAndSetIfChanged(ref _department, value);
+        }
+
+        public StoreViewModel() { }
+    }
+
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Item Item1 { get; set; }
+        public Item Item2 { get; set; }
+        public Item Item3 { get; set; }
+        public Item Item4 { get; set; }
+
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly CreatedDate { get; set; }
+        public IObservableCollection<Location> SoldAtLocations { get; set; }
+    }
+
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+
+    }
+}
---
+++ Views/StoreView.razor.cs
@@ -0,0 +1,14 @@
+﻿using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using ServerSideExample.ViewModels;
+
+namespace ServerSideExample.Views
+{
+    public partial class StoreView
+    {
+        public StoreView()
+        {
+            ViewModel = new StoreViewModel();
+        }
+    }
+}
