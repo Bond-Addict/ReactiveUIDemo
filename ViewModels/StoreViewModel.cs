6cc3f77860d8df26ca1e7be3e0219de529d46fc1
Bond-Addict <117455332+Bond-Addict@users.noreply.github.com>
2025-01-09 21:43:47 -0500

    Simple store project for learning ReactiveUI

--- ViewModels/StoreViewModel.cs
+++ ViewModels/StoreViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Reactive;
-using System.Reactive.Linq;
+using Bogus;
 using DynamicData.Binding;
 using ReactiveUI;
 
@@ -16,7 +15,52 @@ namespace ServerSideExample.ViewModels
             set => this.RaiseAndSetIfChanged(ref _department, value);
         }
 
-        public StoreViewModel() { }
+        public StoreViewModel() {
+            //I assume I need to do something here.....
+        }
+
+        public StoreViewModel Initialize()
+        {
+            var locationFaker = new Faker<Location>()
+         .RuleFor(l => l.Id, f => f.IndexFaker + 1)
+         .RuleFor(l => l.Name, f => f.Address.City())
+         .RuleFor(l => l.State, f => f.Address.State());
+
+            // Faker for Item
+            var itemFaker = new Faker<Item>()
+                .RuleFor(i => i.Id, f => f.IndexFaker + 1)
+                .RuleFor(i => i.Name, f => f.Commerce.ProductName())
+                .RuleFor(i => i.CreatedDate, f => DateOnly.FromDateTime(f.Date.Past()))
+                .RuleFor(i => i.SoldAtLocations, f =>
+                {
+                    var locations = new ObservableCollectionExtended<Location>();
+                    locations.AddRange(locationFaker.Generate(f.Random.Int(1, 5))); // Random number of locations
+                    return locations;
+                });
+
+            // Faker for Department
+            var departmentFaker = new Faker<Department>()
+                .RuleFor(d => d.Id, f => f.IndexFaker + 1)
+                .RuleFor(d => d.Name, f => f.Commerce.Department())
+                .RuleFor(d => d.Item1, f => itemFaker.Generate())
+                .RuleFor(d => d.Item2, f => itemFaker.Generate())
+                .RuleFor(d => d.Item3, f => itemFaker.Generate())
+                .RuleFor(d => d.Item4, f => itemFaker.Generate());
+
+            // Faker for StoreViewModel
+            var storeViewModelFaker = new Faker<StoreViewModel>()
+                .RuleFor(s => s.Departments, f =>
+                {
+                    var departments = new ObservableCollectionExtended<Department>();
+                    departments.AddRange(departmentFaker.Generate(f.Random.Int(2, 5))); // Random number of departments
+                    return departments;
+                });
+
+            // Generate the StoreViewModel with fake data
+            var fakeStore = storeViewModelFaker.Generate();
+
+            return fakeStore;
+        }
     }
 
 
--- Views/StoreView.razor.cs
+++ Views/StoreView.razor.cs
@@ -1,14 +1,24 @@
-﻿using System.Reactive.Threading.Tasks;
-using System.Threading.Tasks;
-using ServerSideExample.ViewModels;
+﻿using ServerSideExample.ViewModels;
 
 namespace ServerSideExample.Views
 {
     public partial class StoreView
     {
+        public bool ShouldShowReportButton { get; set; }
         public StoreView()
         {
-            ViewModel = new StoreViewModel();
+            ViewModel = new StoreViewModel().Initialize();
         }
+
+        protected override void OnInitialized()
+        {
+            //Basically anytime any property contained in the StoreViewModel changes, I want to hide the report button
+            ViewModel.Departments.CollectionChanged += (_, _) => HideButton();
+            base.OnInitialized();
+        }
+
+        public void HideButton() => ShouldShowReportButton = false;
+
+        public void Submit() => ShouldShowReportButton = true;
     }
 }
